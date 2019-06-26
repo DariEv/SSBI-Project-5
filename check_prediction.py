70a19c476055fbe8ba2a5f2b14ea646c79aead98
Florian.Oliver Koenig <f_koenig14@cs.uni-kl.de>
2019-06-26 23:15:20 +0200

    added comments to check predicitons

--- check_prediction.py
+++ check_prediction.py
@@ -1,32 +1,34 @@
-from matplotlib import pyplot as plt
-
-from sklearn.model_selection import cross_val_score, GridSearchCV
-from sklearn.metrics import make_scorer, accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
-
-import os
-import numpy as np
 import pickle
 
 from SOV import calculate_sov
 from features_extractor import FeatureExtractor
 from SOV import calculate_sov
 
+# This file is used to evaluate the performance of a saved pickle model on new unseen test data by calculating the
+# accuracy and the SOV score
+
 
 def main():
+    # Create the Features from the given test set
     fe = FeatureExtractor('supplementary_test/')
 
+    # Path to the pickle file that contains the model we want to evaluate
     model_pickle = './best_models/KNN/knn_q3.pkl'
 
+    # open the model file
     with open(model_pickle, 'rb') as mod_file:
+        # load the model
         model = pickle.load(mod_file)
+        # get the accuracy on the unseen data
         score = model.best_estimator_.score(fe.normalized_features, fe.labels_q3)
-        print(score)
+        print('Accuracy: ',score)
+        # get the actual prediction of the unseen data
         prediction = model.best_estimator_.predict(fe.normalized_features)
-        print(list(prediction))
-        print(fe.labels_q3)
-
-        print(calculate_sov(prediction, fe.labels_q3))
+        # print and calculate the SOV score
+        print('SOV: ', calculate_sov(prediction, fe.labels_q3))
+        print('Predictions:\n')
+        print('Predicted labels: ',list(prediction))
+        print('True labels: ',fe.labels_q3)
 
 
 if __name__ == '__main__':
